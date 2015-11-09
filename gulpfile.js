d8f524f0d2b66cd723ac86aa20255b0b03c8d428
AlexanderPodvoyski <alexander.podvoyski@upsilonit.com>
2015-11-09 16:35:04 +0300

    install glup-include

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    inlineCss = require('gulp-inline-css');
+    inlineCss = require('gulp-inline-css'),
+    include = require("gulp-include");
 
 gulp.task('inline_styles', function() {
     return gulp.src('./before_inliner/*.html')
@@ -13,7 +14,15 @@ gulp.task('inline_styles', function() {
         .pipe(gulp.dest('after_inliner/'));
 });
 
+gulp.task('IncludeFile', function() {
+    return gulp.src('./before_inliner/template.html')
+        // options is optional
+        .pipe(include())
+            .on('error', console.log)
+        .pipe(gulp.dest('after_inliner/'));
+});
 
+/*
 // Rerun the task when a file changes
 gulp.task('watch', function() {
     gulp.watch(['./before_inliner/**'], ['inline_styles']);
@@ -21,3 +30,5 @@ gulp.task('watch', function() {
 
 // The default task (called when you run `gulp` from cli)
 gulp.task('default', ['watch', 'inline_styles']);
+*/
+gulp.task('default', ['IncludeFile']);
