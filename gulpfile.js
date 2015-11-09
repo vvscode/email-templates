223249dd8ad9f89c712ac79a0520b91720d3c561
AlexanderPodvoyski <alexander.podvoyski@upsilonit.com>
2015-11-09 18:26:02 +0300

    Decomposition template modules

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
 
 gulp.task('inline_styles', function() {
     return gulp.src('./before_inliner/*.html')
+        .pipe(include())
+            .on('error', console.log)
         .pipe(inlineCss({
             applyStyleTags: true,
             applyLinkTags: true,
@@ -11,18 +13,11 @@ gulp.task('inline_styles', function() {
             removeLinkTags: true,
             preserveMediaQueries: true
         }))
-        .pipe(gulp.dest('after_inliner/'));
-});
-
-gulp.task('IncludeFile', function() {
-    return gulp.src('./before_inliner/template.html')
-        // options is optional
-        .pipe(include())
             .on('error', console.log)
         .pipe(gulp.dest('after_inliner/'));
 });
 
-/*
+
 // Rerun the task when a file changes
 gulp.task('watch', function() {
     gulp.watch(['./before_inliner/**'], ['inline_styles']);
@@ -30,5 +25,3 @@ gulp.task('watch', function() {
 
 // The default task (called when you run `gulp` from cli)
 gulp.task('default', ['watch', 'inline_styles']);
-*/
-gulp.task('default', ['IncludeFile']);
