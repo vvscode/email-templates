9991087e25159c4f20ac4ec922f8d69c63dd54d5
AlexanderPodvoyski <alexander.podvoyski@upsilonit.com>
2015-11-24 13:50:01 +0300

    next step

---
+++ data.js
@@ -0,0 +1,20 @@
+var DATA_ORIGINAL = {
+/*invitation_viewing_room*/
+"tpl1": {
+"title": 'Invitation to private viewing room',
+"intro": 'Hello, you have been invited to private viewing room by ',
+"recipient_first_name": 'Boris',
+"recipient_last_name": 'Pevzner',
+item: {
+"img": 'http://richardfox.esy.es/img/viewing-room_1@2x.png',
+"img2": 'http://richardfox.esy.es/img/viewing-room_6@2x.png',
+"img3": 'http://richardfox.esy.es/img/viewing-room_5@2x.png',
+"display_name": 'Roy Lichtenstein',
+"description": 'Imperfect 67” x 79 7/8” from Imperfect series”',
+"year": '1988',
+"material": 'Woodcut, screenprint, paper',
+"size": '67 x 79.875 "'
+},
+"button": "GO TO PRIVATE VIEWING ROOM"
+}
+};
